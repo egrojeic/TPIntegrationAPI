a2d9b0c0fca0eff8e271ff98cce96668df25b94b
danilocantor <danilo.cantor.77@gmail.com>
2019-10-03 12:10:11 -0500

    initzialize project

---
+++ IntegrationAPIs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntegrationAPIs.WebApiApplication" Language="C#" %>
---
+++ IntegrationAPIs/Security/TokenGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApiSegura.Security
+{
+
+    internal static class TokenGenerator
+    {
+        public static string GenerateTokenJwt(string username, string rolname)
+        {
+            var secretKey = "20EE6F5DeyJzdWIiOiIxMDE4NDU0MzY5IiwibmFtZSI6IkRhbmlsbyBDYW50yMzkwMjJ9b3IiLCJpYXQiOjE1MTY0253#Mluf75AwBCCG7Ijl64bZRZo7uaQGbpA4AE7oTJUeaCc3v9xADB1&CE7658djclEF71D0";
+            var audienceToken = ConfigurationManager.AppSettings["JWT_AUDIENCE_TOKEN"];
+            var issuerToken = ConfigurationManager.AppSettings["JWT_ISSUER_TOKEN"];
+
+            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(secretKey));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, rolname)
+            });
+
+            var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            var jwtSecurityToken = tokenHandler.CreateJwtSecurityToken(
+                audience: audienceToken,
+                issuer: issuerToken,
+                subject: claimsIdentity,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddMonths(3),
+                signingCredentials: signingCredentials);
+
+            var jwtTokenString = tokenHandler.WriteToken(jwtSecurityToken);
+            return jwtTokenString;
+        }
+    }
+}
